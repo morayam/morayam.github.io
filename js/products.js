09692236d6dd22bc6f018205687509bba7f7c819
morayam <yamila.morales.352@gmail.com>
2022-08-21 17:31:24 -0300

    update products.js

--- js/products.js
+++ js/products.js
@@ -1,20 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(CARS).then(function(resultObj){
-        if(resultObj.status === "ok"){
-            productsList = resultObj.data.products;
-        }
-        showProductsList(productsArray);
-    })
-    })
-    
-    let productsArray = [];
-    
-    function showProducts(array){
-        let htmlContentToAppend = "";
-    
-        for(let i = 0; i < array.length; i++){ 
-            let products = array[i];
-            htmlContentToAppend += `
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(CARS).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      productsArray = resultObj.data.products;
+      showProductsList(productsArray);
+    }
+  });
+});
+function showProducts(array) {
+  let htmlContentToAppend = "";
+
+  for (let i = 0; i < array.length; i++) {
+    let products = array[i];
+    htmlContentToAppend +=
+      `
             <div class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
@@ -23,15 +21,15 @@ document.addEventListener("DOMContentLoaded", function(e){
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
                             <div class="mb-1">
-                            <h4>`+ products.name +`</h4> 
-                            <p> `+ products.description +`</p> 
+                            <h4>` + products.name + `</h4> 
+                            <p> ` + products.description + `</p> 
                             </div>
                             <small class="text-muted">` + products.productCount + ` artículos</small> 
                         </div>
                     </div>
                 </div>
             </div>
-            `
-            document.getElementById("carsList").innerHTML = htmlContentToAppend; 
-        }
-    }
+            `;
+    document.getElementById("carsList").innerHTML = htmlContentToAppend;
+  }
+}
