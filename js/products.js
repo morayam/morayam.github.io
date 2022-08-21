e670f70b2dd293e31958e24a6ac1b68f12f006b7
morayam <yamila.morales.352@gmail.com>
2022-08-21 15:46:33 -0300

    update producstsjs

--- js/products.js
+++ js/products.js
@@ -1,9 +1,37 @@
 document.addEventListener("DOMContentLoaded", function(e){
-getJSONData(CARS).then(function(resultObj){
-    if(resultObj.status === "ok"){
-        carList = resultObj.data.listaDeProductos;
+    getJSONData(CARS).then(function(resultObj){
+        if(resultObj.status === "ok"){
+            carsList = resultObj.data.products;
+        }
+        showProductsList(productsArray);
+    })
+    })
+    
+    let productsArray = [];
+    
+    function showProducts(array){
+        let htmlContentToAppend = "";
+    
+        for(let i = 0; i < array.length; i++){ 
+            let products = array[i];
+            htmlContentToAppend += `
+            <div class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + products.image + `" alt="product image" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <div class="mb-1">
+                            <h4>`+ products.name +`</h4> 
+                            <p> `+ products.description +`</p> 
+                            </div>
+                            <small class="text-muted">` + products.productCount + ` artículos</small> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+            document.getElementById("carsList").innerHTML = htmlContentToAppend; 
+        }
     }
-    console.log(carList);
-})
-})
-
